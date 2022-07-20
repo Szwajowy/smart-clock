495ad3a1b1900815c63d29804cb3e0f2bfcb691b
Szwajowy <krzycza96@gmail.com>
2022-07-20 10:25:01 +0200

    Fix eslint erorrs in dateAgoPipe

--- src/app/shared/pipes/date-ago.pipe.ts
+++ src/app/shared/pipes/date-ago.pipe.ts
@@ -1,5 +1,15 @@
 import { Pipe, PipeTransform } from "@angular/core";
 
+interface TimeUnitValues<T> {
+  year: T;
+  month: T;
+  week: T;
+  day: T;
+  hour: T;
+  minute: T;
+  second: T;
+}
+
 @Pipe({
   name: "dateAgo",
 })
@@ -11,7 +21,7 @@ export class DateAgoPipe implements PipeTransform {
       // if (seconds < 29)
       //   return "właśnie teraz";
 
-      const intervals = {
+      const intervals: TimeUnitValues<number> = {
         year: 31536000,
         month: 2592000,
         week: 604800,
@@ -20,7 +30,8 @@ export class DateAgoPipe implements PipeTransform {
         minute: 60,
         second: 1,
       };
-      const translations = {
+
+      const translations: TimeUnitValues<string[]> = {
         year: ["rok", "lata", "lat"],
         month: ["miesiąc", "miesiące", "miesięcy"],
         week: ["tydzień", "tygodnie", "tygodni"],
@@ -32,18 +43,30 @@ export class DateAgoPipe implements PipeTransform {
 
       let counter;
       for (const i in intervals) {
-        counter = Math.floor(seconds / intervals[i]);
+        counter = Math.floor(
+          seconds / intervals[i as keyof typeof translations]
+        );
         if (counter > 0)
           if (counter === 1) {
-            return translations[i][0] + " temu"; // singular (1 day ago)
+            return translations[i as keyof typeof translations][0] + " temu"; // singular (1 day ago)
           } else {
             switch (counter) {
               case 2:
               case 3:
               case 4:
-                return counter + " " + translations[i][1] + " temu";
+                return (
+                  counter +
+                  " " +
+                  translations[i as keyof typeof translations][1] +
+                  " temu"
+                );
               default:
-                return counter + " " + translations[i][2] + " temu";
+                return (
+                  counter +
+                  " " +
+                  translations[i as keyof typeof translations][2] +
+                  " temu"
+                );
             }
           }
       }
