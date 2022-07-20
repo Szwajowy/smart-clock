6663a3e5c45034c9cb9650abb87109ac13cc4ee6
Szwajowy <krzycza96@gmail.com>
2022-07-20 10:46:04 +0200

    Add environment config for prod

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,20 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBgdhXPiADyIvswqgwgwpvwuSpMg9d9LuY",
+    authDomain: "smartclock-api.firebaseapp.com",
+    databaseURL: "https://smartclock-api.firebaseio.com",
+    projectId: "smartclock-api",
+    storageBucket: "smartclock-api.appspot.com",
+    messagingSenderId: "212228267803",
+    appId: "1:212228267803:web:139f0935949b171239fdbe",
+    measurementId: "G-6X70N876LY",
+  },
+  openWeather: {
+    apiKey: "7d83068566f71e51d798a6cf184d000c",
+    apiURL: "https://api.openweathermap.org",
+  },
+  helperApi: {
+    url: "http://localhost:8080",
+  },
+};
