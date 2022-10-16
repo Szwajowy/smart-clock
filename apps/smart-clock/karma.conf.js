1aebb7fbf9337fd4591fadaa4e6a22802ec501c9
Szwajowy <krzycza96@gmail.com>
2022-10-16 18:27:08 +0200

    Create new configuration for karma

--- apps/smart-clock/karma.conf.js
+++ apps/smart-clock/karma.conf.js
@@ -1,38 +1,14 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
+const path = require('path');
 
-module.exports = function (config) {
+const getBaseKarmaConfig = require('../../karma.conf');
+
+module.exports = (config) => {
+  const baseConfig = getBaseKarmaConfig();
   config.set({
-    basePath: "",
-    frameworks: ["jasmine", "@angular-devkit/build-angular"],
-    plugins: [
-      require("karma-jasmine"),
-      require("karma-chrome-launcher"),
-      require("karma-jasmine-html-reporter"),
-      require("karma-coverage-istanbul-reporter"),
-      require("@angular-devkit/build-angular/plugins/karma"),
-    ],
-    client: {
-      clearContext: false, // leave Jasmine Spec Runner output visible in browser
-    },
+    ...baseConfig,
     coverageIstanbulReporter: {
-      dir: require("path").join(__dirname, "./coverage/smart-clock"),
-      reports: ["html", "lcovonly", "text-summary"],
-      fixWebpackSourcePaths: true,
+      ...baseConfig.coverageIstanbulReporter,
+      dir: path.join(__dirname, './coverage/smart-clock'),
     },
-    reporters: ["progress", "kjhtml"],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ["Chrome"],
-    customLaunchers: {
-      ChromeHeadlessCustom: {
-        base: "ChromeHeadless",
-        flags: ["--no-sandbox", "--disable-gpu"],
-      },
-    },
-    singleRun: false,
-    restartOnFileChange: true,
   });
 };
--- karma.conf.js
+++ karma.conf.js
@@ -1,43 +1,34 @@
+// karma.conf.js
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-const { join } = require('path');
 const { constants } = require('karma');
+const path = require('path');
 
-module.exports = () => {
-  return {
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma'),
-    ],
-    client: {
-      jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
-      },
-      clearContext: false, // leave Jasmine Spec Runner output visible in browser
-    },
-    jasmineHtmlReporter: {
-      suppressAll: true, // removes the duplicated traces
-    },
-    coverageReporter: {
-      dir: join(__dirname, './coverage'),
-      subdir: '.',
-      reporters: [{ type: 'html' }, { type: 'text-summary' }],
-    },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: constants.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: true,
-  };
-};
+module.exports = () => ({
+  basePath: '',
+  frameworks: ['jasmine', '@angular-devkit/build-angular'],
+  plugins: [
+    require('karma-jasmine'),
+    require('karma-chrome-launcher'),
+    require('karma-jasmine-html-reporter'),
+    require('karma-coverage-istanbul-reporter'),
+    require('@angular-devkit/build-angular/plugins/karma'),
+  ],
+  client: {
+    clearContext: false, // leave Jasmine Spec Runner output visible in browser
+  },
+  coverageIstanbulReporter: {
+    dir: path.join(__dirname, 'coverage'),
+    reports: ['html', 'lcovonly', 'text-summary'],
+    fixWebpackSourcePaths: true,
+  },
+  reporters: ['progress', 'kjhtml'],
+  port: 9876,
+  colors: true,
+  logLevel: constants.LOG_INFO,
+  autoWatch: true,
+  browsers: ['Chrome'],
+  singleRun: false,
+  restartOnFileChange: true,
+});
