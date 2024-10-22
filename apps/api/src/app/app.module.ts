004f6d794c4ed93b4d708e0d0b418864d2792c19
Szwajowy <krzycza96@gmail.com>
2024-10-22 12:49:03 +0200

    Connect to DB

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { DevicesModule } from './modules/devices/devices.module';
+import { DataSource } from 'typeorm';
 
 @Module({
-  imports: [DevicesModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: process.env.MONGODB_CONNECTION_STRING,
+      database: process.env.MONGODB_DATABASE,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    }),
+    DevicesModule,
+  ],
   controllers: [],
   providers: [],
 })
